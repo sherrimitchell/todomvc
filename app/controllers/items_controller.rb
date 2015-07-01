01eb7172569500729df88f40beaf9cbd3300f63b
Sherri Mitchell <rsamitchell@att.net>
2015-07-01 19:06:04 -0400

    Adding list controller, and views.

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,21 @@
+class ItemsController < ActionController::Base
+  before_action :authenticate_user!
+
+  def new
+ end
+
+ def create
+ end
+
+ def show
+ end
+
+ def edit
+ end
+
+ def update
+ end
+
+ def delete
+ end
+end
